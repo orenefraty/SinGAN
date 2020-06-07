e6ee2e6ac2b3eb44833b0e8fa819a05212922f70
Oren Efraty <oren.efraty@gmail.com>
2020-06-07 15:41:01 +0000

    style_idx to G

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -251,7 +251,7 @@ def draw_concat(Gs,Zs,reals,NoiseAmp,in_s,mode,m_noise,m_image,opt, style_idx):
                 G_z = G_z[:, :, 0:real_curr.shape[2], 0:real_curr.shape[3]]
                 G_z = m_image(G_z)
                 z_in = noise_amp*Z_opt+G_z
-                G_z = G(z_in.detach(),G_z)
+                G_z = G(z_in.detach(),G_z, style_idx)
                 G_z = imresize(G_z,1/opt.scale_factor,opt)
                 G_z = G_z[:,:,0:real_next.shape[2],0:real_next.shape[3]]
                 count += 1
