e77e72c04372df861c1a60c17ecac3676b0169ed
Noa <noasi131@gmail.com>
2020-06-28 15:31:55 +0300

    Paint2image conditional edit

--- paint2image.py
+++ paint2image.py
@@ -9,7 +9,9 @@ import SinGAN.functions as functions
 if __name__ == '__main__':
     parser = get_arguments()
     parser.add_argument('--input_dir', help='input image dir', default='Input/Images')
-    parser.add_argument('--input_name', help='training image name', required=True)
+    #parser.add_argument('--input_name', help='training image name', required=True)
+    parser.add_argument('--input_name1', help='input image name1', required=True)
+    parser.add_argument('--input_name2', help='input image name2', required=True)
     parser.add_argument('--ref_dir', help='input reference dir', default='Input/Paint')
     parser.add_argument('--ref_name', help='reference image name', required=True)
     parser.add_argument('--paint_start_scale', help='paint injection scale', type=int, required=True)
@@ -29,23 +31,27 @@ if __name__ == '__main__':
             os.makedirs(dir2save)
         except OSError:
             pass
-        real = functions.read_image(opt)
-        real = functions.adjust_scales2image(real, opt)
-        Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
+        #real = functions.read_image(opt)
+        #real = functions.adjust_scales2image(real, opt)
+        #Gs, Zs, reals, NoiseAmp = functions.load_trained_pyramid(opt)
+        real1, real2 = functions.read_image(opt)
+        real1 = functions.adjust_scales2image(real1, opt)
+        real2 = functions.adjust_scales2image(real2, opt)
+        Gs, Zs, reals1, reals2, NoiseAmp = functions.load_trained_pyramid(opt)
         if (opt.paint_start_scale < 1) | (opt.paint_start_scale > (len(Gs)-1)):
             print("injection scale should be between 1 and %d" % (len(Gs)-1))
         else:
             ref = functions.read_image_dir('%s/%s' % (opt.ref_dir, opt.ref_name), opt)
-            if ref.shape[3] != real.shape[3]:
-                ref = imresize_to_shape(ref, [real.shape[2], real.shape[3]], opt)
-                ref = ref[:, :, :real.shape[2], :real.shape[3]]
+            if ref.shape[3] != real1.shape[3]:
+                ref = imresize_to_shape(ref, [real1.shape[2], real1.shape[3]], opt)
+                ref = ref[:, :, :real1.shape[2], :real1.shape[3]]
 
-            N = len(reals) - 1
+            N = len(reals1) - 1
             n = opt.paint_start_scale
             in_s = imresize(ref, pow(opt.scale_factor, (N - n + 1)), opt)
-            in_s = in_s[:, :, :reals[n - 1].shape[2], :reals[n - 1].shape[3]]
+            in_s = in_s[:, :, :reals1[n - 1].shape[2], :reals1[n - 1].shape[3]]
             in_s = imresize(in_s, 1 / opt.scale_factor, opt)
-            in_s = in_s[:, :, :reals[n].shape[2], :reals[n].shape[3]]
+            in_s = in_s[:, :, :reals1[n].shape[2], :reals1[n].shape[3]]
             if opt.quantization_flag:
                 opt.mode = 'paint_train'
                 dir2trained_model = functions.generate_dir2save(opt)
@@ -64,5 +70,7 @@ if __name__ == '__main__':
                 else:
                     train_paint(opt, Gs, Zs, reals, NoiseAmp, centers, opt.paint_start_scale)
                     opt.mode = 'paint2image'
-            out = SinGAN_generate(Gs[n:], Zs[n:], reals, NoiseAmp[n:], opt, in_s, n=n, num_samples=1)
-            plt.imsave('%s/start_scale=%d.png' % (dir2save, opt.paint_start_scale), functions.convert_image_np(out.detach()), vmin=0, vmax=1)
+            out1 = SinGAN_generate(Gs[n:], Zs[n:], reals1, NoiseAmp[n:], opt, [1,0], in_s, n=n, num_samples=1)
+            out2 = SinGAN_generate(Gs[n:], Zs[n:], reals2, NoiseAmp[n:], opt, [0,1], in_s, n=n, num_samples=1)
+            plt.imsave('%s/img1_start_scale=%d.png' % (dir2save, opt.paint_start_scale), functions.convert_image_np(out1.detach()), vmin=0, vmax=1)
+            plt.imsave('%s/img2_start_scale=%d.png' % (dir2save, opt.paint_start_scale), functions.convert_image_np(out2.detach()), vmin=0, vmax=1)
